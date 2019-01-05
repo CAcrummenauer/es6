a3d229af7d7aedfa04ed0892ac2db241ae2b4b84
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-04 23:11:01 -0200

    Mais sobre arrow-const ao invés de function...

--- bundle.js
+++ bundle.js
@@ -1,12 +1,41 @@
 "use strict";
 
 function testA() {
-  return 'testA';
+  return 'TestA';
 }
 
 var testB = function testB() {
-  return 'testB';
+  return 'TestB';
+};
+
+var testC = function testC() {
+  return 'TestC';
+};
+
+var testD = function testD() {
+  name: 'San';
+}; // Não funciona...
+
+
+var testE = function testE() {
+  return {
+    name: 'San',
+    age: 18
+  };
+}; // Precisa ser assim...
+// ou assim: 
+
+
+var testF = function testF() {
+  return {
+    name: 'Lara',
+    age: 19
+  };
 };
 
 console.log(testA());
 console.log(testB());
+console.log(testC());
+console.log(testD());
+console.log(testE());
+console.log(testF());
--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 function testA() {
-    return 'testA';
+    return 'TestA';
 }
 
 const testB = () => {
-    return 'testB';
+    return 'TestB';
 }
+
+const testC = () => 'TestC';
+
+const testD = () => {name: 'San'}; // Não funciona...
+
+const testE = () => {
+    return {name: 'San', age: 18};
+}; // Precisa ser assim...
+
+// ou assim: 
+
+const testF = () => ({name: 'Lara', age: 19});
 
 console.log(testA());
 console.log(testB());
+console.log(testC());
+console.log(testD());
+console.log(testE());
+console.log(testF());

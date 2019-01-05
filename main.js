e370b8032e3a6ece3848f02fa81367237afbaab3
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-04 22:05:16 -0200

    Mutação em constantes...

--- main.js
+++ main.js
@@ -1,41 +1,5 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
+const user = {name: 'Diego'};
 
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
-    }
-}
+user.name = 'Cezar';
 
-class TodoList extends List {
-    constructor() {
-        super();
-        this.user = 'Cezar Augusto';
-    }
-
-    showUser() {
-        console.log(this.user);
-    }
-}
-
-var minhaLista = new TodoList();
-
-document.getElementById('newTodo').onclick = function() {
-    minhaLista.add('Nova tarefa');
-}
-
-minhaLista.showUser();
-
-class TestStatic {
-    constructor() {
-        this.data = [];
-    }
-
-    static add() {
-        this.data.push('Tentativa de entrada...');
-    }
-}
-
-TestStatic.add();
+console.log(user);

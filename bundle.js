b3acb4029468643e815abdfe5715abb8a3051802
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-05 02:29:26 -0200

    Usando template literals...

--- bundle.js
+++ bundle.js
@@ -1,23 +1,7 @@
 "use strict";
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-// Spread
-var array1 = [1, 2, 3];
-var array2 = [4, 5, 6];
-var array3 = [].concat(array1, array2);
-console.log(array3);
-var user1 = {
-  name: 'Diego',
-  idade: 23,
-  empresa: 'RocketSeat'
-};
-console.log(user1);
-
-var user2 = _objectSpread({}, user1, {
-  name: 'Gabriel'
-});
-
-console.log(user2);
+// Template literals
+var name = 'Cezar';
+var age = 23;
+console.log('Meu nome é ' + name + ' e tenho idade: ' + age + ' anos.');
+console.log("Meu nome \xE9 ".concat(name, " e tenho idade: ").concat(age, " anos."));
--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-// Spread
-const array1 = [1, 2, 3];
-const array2 = [4, 5, 6];
-const array3 = [...array1, ...array2];
-console.log(array3);
-
-const user1 = {
-    name: 'Diego',
-    idade: 23,
-    empresa: 'RocketSeat'
-};
-console.log(user1);
-const user2 = {...user1, name: 'Gabriel'};
-console.log(user2);
+// Template literals
+const name = 'Cezar';
+const age = 23;
+console.log('Meu nome é ' + name + ' e tenho idade: ' + age + ' anos.');
+console.log(`Meu nome é ${name} e tenho idade: ${age} anos.`);

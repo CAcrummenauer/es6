671463d2678b9e085597096b8a844e5415f2f080
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-04 22:27:16 -0200

    Trabalhando com map em um array e usando o index...

--- bundle.js
+++ bundle.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var array = [1, 2, 3, 4, 5, 6, 7];
-var newArray = array.map(function (item) {
-  return item * 2;
+var newArray = array.map(function (item, index) {
+  return item + index;
 });
 console.log(newArray);
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
-const newArray = array.map(function(item) {
-    return item * 2;
+const newArray = array.map(function(item, index) {
+    return item + index;
 });
 
 console.log(newArray);

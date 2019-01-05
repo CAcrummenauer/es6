b3093f9190cd6ae5eda3e6486f8df74e187ff515
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-04 22:59:37 -0200

    Usando const ao invés de function...

--- bundle.js
+++ bundle.js
@@ -1,19 +1,12 @@
 "use strict";
 
-var array = [1, 2, 3, 4, 5, 6, 7, 4];
-var newArrayA = array.map(function (item, index) {
-  return item + index;
-});
-console.log(newArrayA);
-var newArrayB = array.map(function (item, index) {
-  return item + index;
-});
-console.log(newArrayB);
-var newArrayC = array.map(function (item) {
-  return item * 10;
-});
-console.log(newArrayC);
-var newArrayD = array.map(function (item) {
-  return item * 2;
-});
-console.log(newArrayD);
+function testA() {
+  return 'testA';
+}
+
+var testB = function testB() {
+  return 'testB';
+};
+
+console.log(testA());
+console.log(testB());
--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-const array = [1, 2, 3, 4, 5, 6, 7, 4];
+function testA() {
+    return 'testA';
+}
 
-const newArrayA = array.map(function(item, index) {
-    return item + index;
-});
-console.log(newArrayA);
+const testB = () => {
+    return 'testB';
+}
 
-const newArrayB = array.map((item, index) => {
-    return item + index;
-});
-console.log(newArrayB);
-
-const newArrayC = array.map(item => {
-    return item * 10;
-});
-console.log(newArrayC);
-
-const newArrayD = array.map(item => item * 2);
-console.log(newArrayD);
+console.log(testA());
+console.log(testB());

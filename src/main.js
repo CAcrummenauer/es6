c58e4c1ca8062d6174670b3d8a52aae230b62eaf
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-06 05:34:44 -0200

    Renderizando elementos na página...

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ class App {
     constructor() {
         this.repositories = [];
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
         this.registerHandlers();
     }
 
@@ -12,12 +13,38 @@ class App {
     addRepository(event) {
         event.preventDefault();
         this.repositories.push({
-            name: 'name',
-            description: 'description',
-            avatarUrl: 'avatarUrl',
-            htmlUrl: 'htmlUrl'
+            name: 'Rocketseat',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+            avatarUrl: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            htmlUrl: 'https://github.com/Rocketseat'
         });
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatarUrl);
+            
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 const app = new App();

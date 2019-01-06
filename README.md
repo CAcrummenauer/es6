d00c8f2924baeb60658a1a08b4c7272a1c5d25fe
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-06 04:39:34 -0200

    Trabalhando com async/await (try-catch) para consumir uma API, axios...

--- README.md
+++ README.md
@@ -127,3 +127,5 @@ module.exports = {
         ]
     }
 };
+
+yarn add axios
--- src/main.js
+++ src/main.js
@@ -1,36 +1,14 @@
-const myPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => {resolve('OK')}, 2000);
-});
-myPromise().then(response => {
-    console.log(response);
-})
-.catch(err => {
-    console.log(err);
-});
+import axios from 'axios';
 
-async function runPromiseA() {
-    // Usar await apenas dentro de uma função async
-    console.log('Async/await: ' + await myPromise());
-    console.log('Async/await: ' + await myPromise());
-    console.log('Async/await: ' + await myPromise());
-    
-    // Using only promise:
-    myPromise().then(response => {
-        console.log('Only promise: ' + response);
-        myPromise().then(response => {
-            console.log('Only promise: ' + response);
-            myPromise().then(response => {
-                console.log('Only promise: ' + response);
-            });
-        });
-    });
-};
-runPromiseA();
+class Api {
+    static async getUserInfo(username) {
+        try {
+            const response = await axios.get(`http://api.github.com/users/${username}`);
+            console.log(response);
+        } catch (err) {
+            console.warn('Erro ao consumir API: ' + err);
+        }
+    }
+}
 
-const runPromiseB = async() => {
-    console.log('Async/await with arrow: ' + await myPromise());
-    console.log('Async/await with arrow: ' + await myPromise());
-    console.log('Async/await with arrow: ' + await myPromise());
-};
-
-runPromiseB();
+Api.getUserInfo('cacrummenauer');

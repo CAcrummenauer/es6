253c2b8adf61c9097cc39a45b8e56aab82fdf3c1
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-06 03:56:13 -0200

    Começando com async/await...

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('lala');
+const myPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => {resolve('OK')}, 2000);
+});
+
+myPromise().then(response => {
+    console.log(response);
+})
+.catch(err => {
+    console.log(err);
+});

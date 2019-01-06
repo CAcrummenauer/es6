421e91bab12fe0c2facd820a7d1411b98699a2a6
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-06 04:23:30 -0200

    Trabalhando de várias maneira com async/await...

--- src/main.js
+++ src/main.js
@@ -8,8 +8,29 @@ myPromise().then(response => {
     console.log(err);
 });
 
-async function runPromise() {
-    const response = await myPromise();
-    console.log(response);
-}
-runPromise();
+async function runPromiseA() {
+    // Usar await apenas dentro de uma função async
+    console.log('Async/await: ' + await myPromise());
+    console.log('Async/await: ' + await myPromise());
+    console.log('Async/await: ' + await myPromise());
+    
+    // Using only promise:
+    myPromise().then(response => {
+        console.log('Only promise: ' + response);
+        myPromise().then(response => {
+            console.log('Only promise: ' + response);
+            myPromise().then(response => {
+                console.log('Only promise: ' + response);
+            });
+        });
+    });
+};
+runPromiseA();
+
+const runPromiseB = async() => {
+    console.log('Async/await with arrow: ' + await myPromise());
+    console.log('Async/await with arrow: ' + await myPromise());
+    console.log('Async/await with arrow: ' + await myPromise());
+};
+
+runPromiseB();
